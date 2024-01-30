3e04fbcdacb42d62f1bbd218dde82af6a97aa325
Peter Richards <pejrich@gmail.com>
2024-01-30 12:35:14 -0800

    v0.3.0

--- lib/charex/native.ex
+++ lib/charex/native.ex
@@ -1,11 +1,12 @@
 defmodule Charex.Native do
   @moduledoc false
   version = Mix.Project.config()[:version]
+  github_url = Mix.Project.config()[:package][:source_url]
 
   use RustlerPrecompiled,
     otp_app: :charex,
     crate: "charex_native",
-    base_url: "https://github.com/pejrich/charex/releases/download/v#{version}",
+    base_url: "#{github_url}/releases/download/v#{version}",
     force_build: System.get_env("CHAREX_BUILD") in ["1", "true"],
     version: version
 
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Charex.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.3.0"
   @source_url "https://github.com/pejrich/charex"
 
   def project do
